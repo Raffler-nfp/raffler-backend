17cccb5697e34399d6708544f2fadf82ed053953
ShreyKhanna <shrey_khanna@ymail.com>
2019-08-11 11:30:15 +1000

    Backend code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app=require('express')();
 const cors=require('cors')
 const body_parser=require('body-parser')
 const mongoose=require('mongoose') 
-var userRouter=require('./routes/signup.route')
+var userRouter=require('./routes/user.route')
 //var app=express()
 require('dotenv').config();
 const port=process.env.PORT || 5000
@@ -14,8 +14,6 @@ const connection=mongoose.connection;
 connection.once('open',()=>{
     console.log("Mongodb connected succesfully")
 })
-
-
 app.use('/users',userRouter)
 app.listen(port,()=>{
     console.log(`Connected successfully:${port}`);
